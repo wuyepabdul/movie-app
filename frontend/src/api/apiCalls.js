761f1b9099ca5db26336d9d78c39698e193f28b5
wuyepabdul <wuyepabdul@gmail.com>
2025-10-24 11:46:24 +0100

    Setup Api call to upcoming movie for hero section

---
+++ frontend/src/api/apiCalls.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const getToken = (token) => ({
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTJkNzQ2M2FiNDllYmNiY2Y1M2NhYjAyMzUzNTI1YSIsIm5iZiI6MTc2MTIxNjE5Ny43NjYsInN1YiI6IjY4ZmEwNmM1NzExZDBlNGY5ZGRkOTMzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JiotyQlMu6MXlFF5aGjOBTN3E0QdsbSTY99YA_YE2UM",
+    "Content-Type": "application/json",
+  },
+});
+
+export const getUpcomingMovies = async () => {
+  try {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1"
+    );
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log("error in upcoming movies", error);
+    return error;
+  }
+};
--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,26 +1,47 @@
-import React from "react";
-import HeroBg from "../assets/sinners.jpg";
+import { useEffect, useState } from "react";
 import { Bookmark, Play } from "lucide-react";
-const Hero = () => {
-  return (
-    <div className="text-white relative">
-      <img
-        src={HeroBg}
-        alt="bg-img"
-        className="w-full rounded-2xl h-[480px] object-center object-cover"
-      />
+import { getUpcomingMovies } from "../api/apiCalls";
 
-      <div className="flex space-x-2 md:space-x-4 absolute bottom-3 left-4 md:bottom-8 md:left-10 font-medium">
-        <button className="flex justify-center items-center bg-white hover:bg-gray-200 text-[#c74b09f3] py-3 px-4 rounded-full  cursor-pointer text-sm md:text-base font-bold">
-          <Bookmark className="mr-2 w-4 h-5 md:w-5 md:h-5" />
-          Save for later{" "}
-        </button>
-        <button className="flex justify-center items-center bg-[#c74b09f3] hover:bg-gray-200 text-white py-3 px-4 rounded-full  cursor-pointer text-sm md:text-base font-bold">
-          {" "}
-          <Play className="mr-2 w-4 h-5 md:w-5 md:h-5" /> Watch Now
-        </button>
-      </div>
-    </div>
+const Hero = () => {
+  const [movie, setMovie] = useState(null);
+
+  const fetchAndSetHeroMovie = async () => {
+    const data = await getUpcomingMovies();
+    if (data && data.results.length > 0) {
+      const randomIndex = Math.floor(Math.random() * data.results.length);
+      setMovie(data.results[randomIndex]);
+    }
+  };
+
+  useEffect(() => {
+    fetchAndSetHeroMovie();
+  }, []);
+
+  return (
+    <>
+      {movie ? (
+        <div className="text-white relative">
+          <img
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            alt="bg-img"
+            className="w-full rounded-2xl h-[480px] object-center object-cover"
+          />
+
+          <div className="flex space-x-2 md:space-x-4 absolute bottom-3 left-4 md:bottom-8 md:left-10 font-medium">
+            <button className="flex justify-center items-center bg-white hover:bg-gray-200 text-[#c74b09f3] py-3 px-4 rounded-full  cursor-pointer text-sm md:text-base font-bold">
+              <Bookmark className="mr-2 w-4 h-5 md:w-5 md:h-5" />
+              Save for later{" "}
+            </button>
+            <button className="flex justify-center items-center bg-[#c74b09f3] hover:bg-gray-200 text-white py-3 px-4 rounded-full  cursor-pointer text-sm md:text-base font-bold">
+              {" "}
+              <Play className="mr-2 w-4 h-5 md:w-5 md:h-5" /> Watch Now
+            </button>
+          </div>
+        </div>
+      ) : (
+        "loading"
+      )}
+    </>
   );
 };
 
